59542462ee91cfca11b8bac279a68d8ca17811c6
Elton Nascimento <eltongarbin@gmail.com>
2018-07-21 23:04:22 -0300

    User API Routes  JWT Authentication

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    'mongodb://elton:elton1234@ds153705.mlab.com:53705/devconnector-garbin'
+    'mongodb://elton:elton1234@ds153705.mlab.com:53705/devconnector-garbin',
+  secretOrKey: 'secret'
 };
