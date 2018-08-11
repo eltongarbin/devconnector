9eb3c0e6fafffca5706d56573677482f22f32577
Elton Nascimento <eltongarbin@gmail.com>
2018-08-11 17:26:56 -0300

    Form Field Components

--- client/src/components/create-profile/CreateProfile.jsx
+++ client/src/components/create-profile/CreateProfile.jsx
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import TextFieldGroup from '../common/TextFieldGroup';
+import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
+import InputGroup from '../common/InputGroup';
+import SelectListGroup from '../common/SelectListGroup';
 
 class CreateProfile extends Component {
   static propTypes = {
