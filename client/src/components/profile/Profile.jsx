90a53c5185fde9ac64a080107c478bae0b080bc3
Elton Garbin <eltongarbin@gmail.com>
2018-12-28 17:05:02 -0200

    Profile Header Component

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -25,12 +25,37 @@ class Profile extends PureComponent {
   }
 
   render() {
+    const { profile, loading } = this.props.profile;
+    let profileContent;
+
+    if (profile === null || loading) {
+      profileContent = <Spinner />;
+    } else {
+      profileContent = (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <Link className="btn btn-light mb-3 float-left" to="/profiles">
+                Back to Profiles
+              </Link>
+            </div>
+            <div className="col-md-6" />
+          </div>
+          <ProfileHeader profile={profile} />
+          <ProfileAbout />
+          <ProfileCreds />
+          <ProfileGithub />
+        </div>
+      );
+    }
+
     return (
-      <div>
-        <ProfileHeader />
-        <ProfileAbout />
-        <ProfileCreds />
-        <ProfileGithub />
+      <div className="profile">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">{profileContent}</div>
+          </div>
+        </div>
       </div>
     );
   }
