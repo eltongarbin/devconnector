d1f8a9d576157ed8f5021abd997d6d87ecc3f1ff
Elton Garbin <eltongarbin@gmail.com>
2018-12-29 15:11:24 -0200

    Securing keys

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:
-    'mongodb://elton:elton1234@ds153705.mlab.com:53705/devconnector-garbin',
-  secretOrKey: 'secret'
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
